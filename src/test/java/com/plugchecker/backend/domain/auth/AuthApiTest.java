a6cebdd5064d159c5a3a35afca102edeaff8bde6
dohyeon5626 <kodohyeon71@gmail.com>
2021-07-23 20:32:07 +0900

    [TEST] add testcode

--- src/test/java/com/plugchecker/backend/domain/auth/AuthApiTest.java
+++ src/test/java/com/plugchecker/backend/domain/auth/AuthApiTest.java
@@ -5,19 +5,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.plugchecker.backend.domain.auth.domain.RefreshTokenRepository;
 import com.plugchecker.backend.domain.auth.domain.User;
 import com.plugchecker.backend.domain.auth.domain.UserRepository;
+import com.plugchecker.backend.domain.auth.dto.request.EmailRequest;
 import com.plugchecker.backend.domain.auth.dto.request.RefreshTokenRequest;
 import com.plugchecker.backend.domain.auth.dto.request.SignInRequest;
+import com.plugchecker.backend.domain.auth.dto.request.SignUpRequest;
 import com.plugchecker.backend.domain.auth.dto.response.TokenResponse;
+import com.plugchecker.backend.domain.auth.service.SignUpService;
 import com.plugchecker.backend.global.security.JwtTokenProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AuthApiTest extends AuthApiRequest{
@@ -33,9 +40,54 @@ public class AuthApiTest extends AuthApiRequest{
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private SignUpService signUpService;
+
+    @Test
+    @DisplayName("이메일 인증번호 확인")
+    void checkNumber() throws Exception {
+        // given
+        String email = "xxxxxxx@gmail.com";
+        String number = "9999";
+        String accessToken = "accessToken";
+        String refreshToken = "refreshToken";
+
+        EmailRequest request = new EmailRequest(email, number);
+        TokenResponse response = new TokenResponse(accessToken, refreshToken);
+
+        given(signUpService.checkNumber(any())).willReturn(response);
+
+        // when
+        ResultActions resultActions = requestCheckNumber(request);
+
+        // then
+        resultActions.andExpect(status().isCreated())
+                .andExpect(jsonPath("accessToken").value(accessToken))
+                .andExpect(jsonPath("refreshToken").value(refreshToken))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원가입 이메일 인증번호 발송")
+    void signUp() throws Exception {
+        // given
+        String id = "testUserId";
+        String password = "testUserPassword";
+        String email = "xxxxxxx@gmail.com";
+
+        SignUpRequest request = new SignUpRequest(id, password, email);
+
+        // when
+        ResultActions resultActions = requestSignUp(request);
+
+        // then
+        resultActions.andExpect(status().isOk())
+                .andDo(print());
+    }
+
     @Test
     @DisplayName("로그인")
-    void login_Test() throws Exception {
+    void login() throws Exception {
         // given
         String id = "testUser";
         String password = "password123123#";
@@ -63,7 +115,7 @@ public class AuthApiTest extends AuthApiRequest{
 
     @Test
     @DisplayName("토큰리프레쉬")
-    void tokenRefresh_Test() throws Exception {
+    void tokenRefresh() throws Exception {
         // given
         String id = "testUser";
         String refresh = jwtTokenProvider.generateRefreshToken(id);
